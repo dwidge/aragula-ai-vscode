0b40dd9953d515ab98addd13a6a01a38c3ba1962
dwidge <dwidge+gh@gmail.com>
2025-09-17 14:39:39 +0200

    fix(setCommitMessage): Improve error messages and target path matching

--- src/setCommitMessage.ts
+++ src/setCommitMessage.ts
@@ -5,23 +5,28 @@ export async function setCommitMessage(
   repoPath: string,
   message: string
 ): Promise<void> {
+  const targetPath = `${repoPath.toLowerCase().replaceAll("\\", "/")}`;
+
   const gitExtension =
     vscode.extensions.getExtension<GitExtension>("vscode.git");
   if (!gitExtension) {
     console.warn("Git extension not found.");
-    return;
+    throw new Error("setCommitMessageE1: Git extension not found.");
   }
   const git = gitExtension.exports.getAPI(1);
 
   const targetRepo = git.repositories.find((repo) => {
     const repoRoot = repo.rootUri.path.toLowerCase();
-    const targetPath = `/${repoPath.toLowerCase()}`;
     return repoRoot.endsWith(targetPath);
   });
 
   if (targetRepo) {
     targetRepo.inputBox.value = message;
   } else {
-    console.warn(`Repository not found for path: ${repoPath}`);
+    throw new Error(
+      `setCommitMessageE2: Repository not found for path: ${repoPath}. Available repositories: ${git.repositories
+        .map((r) => r.rootUri.path)
+        .join(", ")}`
+    );
   }
 }
